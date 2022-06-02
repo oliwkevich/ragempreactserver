d9c9789e56e0f67cc09afa4fa1affae69cd0ea09
faceplantDev <75030013+faceplantDev@users.noreply.github.com>
2022-06-02 23:31:57 +0300

    prev comm fix

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./modules/pauseMenu/customPauseLines");
+require("./modules/keys");
+require("./modules/preventGameActions");
+require("./modules/camera");
---
+++ packages/core/modules/weather/index.js
@@ -0,0 +1,76 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_cron_1 = __importDefault(require("node-cron"));
+let getJSON = require("get-json");
+let weatherURL = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/2515327?apikey=e3VlK1UFwNn15SJ6y6EukB2CAJ29wKmJ";
+function updateData() {
+    let date = new Date();
+    let minute = date.getMinutes();
+    let hour = date.getHours();
+    mp.world.time.set(hour, minute, 0);
+    {
+        getJSON(weatherURL, function (error, response) {
+            let weatherInfo = response[0].WeatherIcon;
+            if (weatherInfo == 1) {
+                mp.world.weather = "EXTRASUNNY";
+            }
+            else if (weatherInfo == 2 || weatherInfo == 3 || weatherInfo == 4 || weatherInfo == 30 || weatherInfo == 33 || weatherInfo == 34) {
+                mp.world.weather = "CLEAR";
+            }
+            else if (weatherInfo == 5 ||
+                weatherInfo == 6 ||
+                weatherInfo == 7 ||
+                weatherInfo == 9 ||
+                weatherInfo == 10 ||
+                weatherInfo == 20 ||
+                weatherInfo == 21 ||
+                weatherInfo == 24 ||
+                weatherInfo == 31 ||
+                weatherInfo == 32 ||
+                weatherInfo == 35 ||
+                weatherInfo == 36 ||
+                weatherInfo == 37 ||
+                weatherInfo == 38 ||
+                weatherInfo == 43 ||
+                weatherInfo == 44) {
+                mp.world.weather = "CLOUDS";
+            }
+            else if (weatherInfo == 19) {
+                mp.world.weather = "SMOG";
+            }
+            else if (weatherInfo == 11) {
+                mp.world.weather = "FOGGY";
+            }
+            else if (weatherInfo == 8) {
+                mp.world.weather = "OVERCAST";
+            }
+            else if (weatherInfo == 12 || weatherInfo == 13 || weatherInfo == 14) {
+                mp.world.weather = "CLEARING";
+            }
+            else if (weatherInfo == 18 ||
+                weatherInfo == 22 ||
+                weatherInfo == 23 ||
+                weatherInfo == 25 ||
+                weatherInfo == 26 ||
+                weatherInfo == 27 ||
+                weatherInfo == 28 ||
+                weatherInfo == 29 ||
+                weatherInfo == 39 ||
+                weatherInfo == 40) {
+                mp.world.weather = "RAIN";
+            }
+            else if (weatherInfo == 15 || weatherInfo == 16 || weatherInfo == 17 || weatherInfo == 41 || weatherInfo == 42) {
+                mp.world.weather = "THUNDER";
+            }
+        });
+    }
+    ;
+}
+;
+node_cron_1.default.schedule(`0 0 * * * *`, () => {
+    updateData();
+});
+updateData();
