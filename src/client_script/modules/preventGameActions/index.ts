a6f91338a8d8395139c10721afb98f02291c639e
faceplantDev <75030013+faceplantDev@users.noreply.github.com>
2022-06-02 23:51:28 +0300

    new prevents

--- src/client_script/modules/preventGameActions/index.ts
+++ src/client_script/modules/preventGameActions/index.ts
@@ -4,3 +4,16 @@ setInterval(() => { //AFK CAMERA OFF
 }, 25000);
 
 mp.game.audio.startAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE"); //FULL SILENT(УБИРАЕТ ЗВУК ТАЧЕК И ПОЛИЦИИ)
+
+mp.events.add("render", () => { //селектор оружия и названия улиц + названия класса и названия авто
+    mp.game.ui.hideHudComponentThisFrame(19); // Оружия
+    mp.game.ui.hideHudComponentThisFrame(20);
+    mp.game.ui.hideHudComponentThisFrame(22);
+    mp.game.ui.hideHudComponentThisFrame(6); // названия ниже
+    mp.game.ui.hideHudComponentThisFrame(7);
+    mp.game.ui.hideHudComponentThisFrame(8);
+    mp.game.ui.hideHudComponentThisFrame(9);
+    mp.game.ui.hideHudComponentThisFrame(3); //худ денег
+})
+
+mp.gui.chat.activate(false) //офф чата
